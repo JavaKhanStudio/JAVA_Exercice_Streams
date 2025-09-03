470ef6d9392c18bbb6ff27baf255db1bdc0fa9df
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-09-03 12:00:18 +0200

    Create Cleaner.java

---
+++ src/main/java/sbp/stream/exercices/simple/Cleaner.java
@@ -0,0 +1,19 @@
+package sbp.stream.exercices.simple;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Cleaner {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+
+}
