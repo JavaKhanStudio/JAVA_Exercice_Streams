eea902d3d144c5ecb2b41ca0ab7f5bee5f0b4f25
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-09-09 00:54:49 +0200

    Update Exo2.java

--- src/main/java/sbp/stream/exercices/simple/Exo2.java
+++ src/main/java/sbp/stream/exercices/simple/Exo2.java
@@ -1,6 +1,5 @@
 package sbp.stream.exercices.simple;
 
-import java.util.IntSummaryStatistics;
 import java.util.List;
 
 public class Exo2 {
@@ -40,66 +39,41 @@ public class Exo2 {
         // Expected : 655
 
         // Q1 - Calculer la somme de testingListImbr
-        int q1 = testingListImbr
-                .stream()
-                .flatMapToInt(stream -> stream.stream().mapToInt(Integer::intValue))
-                .sum();
-
-        System.out.println(q1);
+        //int q1 = testingListImbr
+        //System.out.println(q1);
         // Expected : 282
 
         // Q2 - Calculer la somme de tous les nombres plus grand que 50 dans la testingList
-        int q2 = testingList
-                .stream()
-                .mapToInt(Integer::intValue)
-                .filter(x -> x > 50)
-                .sum();
-
-        System.out.println(q2);
+        // int q2 = testingList
+        // System.out.println(q2);
         // Expected : 93
 
         // Q3 - Calculer la moyenne de tous les nombres impaire de la testingListImbr
-        double q3 = testingListImbr
-                .stream()
-                .flatMapToInt(stream -> stream.stream().mapToInt(Integer::intValue))
-                .filter(x -> x % 2 != 0)
-                .average().orElse(0);
+        // double q3 = testingListImbr
 
-        System.out.println(q3);
+
+        // System.out.println(q3);
         // Expected : 26.4
 
         // Q4 Trouver le plus grand nombre qui est un multiple de 5 dans la testingList
-        int q4 = testingList
-                .stream()
-                .mapToInt(Integer::intValue)
-                .filter(x -> x % 5 == 0)
-                .max().orElse(0);
+        // int q4 = testingList
 
-        System.out.println(q4);
+        // System.out.println(q4);
         // Expected : 40
 
         // Q5 - Afficher la somme des élément de la testingDoubleList mise au carré
-        double q5 = testingDoubleList
-                .stream()
-                .map(x -> x * x)
-                .mapToDouble(Double::doubleValue)
-                .sum();
+        // double q5 = testingDoubleList
 
-        System.out.println(q5);
+        // System.out.println(q5);
         // Expected : 275.03000000000003
 
 
         // Q6 - Afficher tous les stats, base sur les nombres unique de la testingList,
         // mise au carré,
         // en excluant tous les éléments plus grand que 50 une fois cette mise au carré effectué
-        IntSummaryStatistics q6 = testingList
-                .stream()
-                .mapToInt(Integer::intValue)
-                .map(x -> x * x)
-                .filter(y -> y < 50)
-                .summaryStatistics();
+        // IntSummaryStatistics q6 = testingList
 
-        System.out.println(q6);
+        // System.out.println(q6);
         // Expected : IntSummaryStatistics{count=11, sum=202, min=1, average=18,363636, max=49}
     }
 
