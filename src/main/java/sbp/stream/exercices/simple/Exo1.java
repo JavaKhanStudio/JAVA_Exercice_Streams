215da70680834d8d8e7c26d8fa94636766d3d15f
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-09-09 10:54:17 +0200

    Update Exo1.java

--- src/main/java/sbp/stream/exercices/simple/Exo1.java
+++ src/main/java/sbp/stream/exercices/simple/Exo1.java
@@ -1,24 +1,21 @@
 package sbp.stream.exercices.simple;
 
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Exo1 {
 
     public static List<Integer> testingList = List.of(
             1, 1, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 9, 9, 9,
             12, 14, 16, 17, 19, 20, 23, 29, 30, 42,
-            11, 33, 22, 22, 11, 22, 93, 12, 34, 40, 39) ;
+            11, 33, 22, 22, 11, 22, 93, 12, 34, 40, 39);
 
     public static List<List<Integer>> testingListImbr = List.of(
-            List.of(1,1,2),
-            List.of(4,5,6),
-            List.of(55,5,1),
-            List.of(0,1,6),
-            List.of(65,65,65)
-    ) ;
+            List.of(1, 1, 2),
+            List.of(4, 5, 6),
+            List.of(55, 5, 1),
+            List.of(0, 1, 6),
+            List.of(65, 65, 65)
+    );
 
     public static void main(String[] args) {
 
@@ -47,7 +44,7 @@ public class Exo1 {
 
         // Expected : 15
 
-        // Q6 - Calculer la somme des nombres unique dans la double liste de nombre (testingListImbr)
+        // Q6 - Retourner le nombre de nombres unique dans la double liste de nombre (testingListImbr)
         // long valueQ6 =
 
         // Expected : 8
